9de0ae65fd03d61a15a761cf1eed0007abbcca70
Peter Ort <peter@peterort.com>
2020-04-23 15:40:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.count <= 2
     array.join(" and ")
   elsif array.count >= 3
-    array[0...-1].join(", ") << array[-1].join(", and")
+    array[0...-1].join(", ") << ", and #{array[-1]}"
   end
 
 end
