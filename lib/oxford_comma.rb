5e51733f828d13e014d0befe49dd9ae953c590f5
Peter Ort <peter@peterort.com>
2020-04-23 15:26:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,14 @@
-def oxford_comma(array)
-  if array.count <= 2
-    array.join(" and ")
-  elsif array.count >= 3
-    array.join(", ")
-    array[-1].join(", and ")
-  end
+#def oxford_comma(array)
+#  if array.count <= 2
+#    array.join(" and ")
+#  elsif array.count >= 3
+#    array.join(", ")
+#    array[-1].join(", and ")
+#  end
+#
+#end
 
+
+def oxford_comma(array)
+  array.to_sentence
 end
